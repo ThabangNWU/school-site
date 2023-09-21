62003b9a725818694fcb919c435a963c3f1455aa
chailock <chailock.sphe@gmail.com>
2023-09-21 15:55:07 +0200

    Aps calculation

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 function App() {
   return (
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </Router>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route part="Aps" element={<Aps />} />
+      </Routes>
+    </Router>
   );
 }
 
