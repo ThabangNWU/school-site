ed92462dc1b0a1dbc9d9255a84c41f6460404c42
ThabangNWU <admorelebele@gmail.com>
2023-09-27 11:05:53 +0200

    user and contact dashboard done

---
+++ src/components/PortalNavBar.tsx
@@ -0,0 +1,24 @@
+import {AiOutlineMenu,AiOutlineDown} from 'react-icons/ai';
+import profilePicture from "../Image/me.jpg"
+
+
+export const PortalNavbar = ()  => {
+
+    return (
+    <div className="navbar">
+        <div className="navbar__container">
+            <div className='split'>
+                <div className="navbar__breadcrumb">
+                    <AiOutlineMenu/>
+                </div>
+                <div className="navbar__profile">
+                    <img src= {profilePicture} className="navbar__profile-picture" alt='image'/>
+                    <p className="navbar__profile_name">Thabang Admore</p>
+                    <AiOutlineDown className="chevron-down"/>
+                </div>
+            </div>
+           
+        </div>
+    </div>
+    )
+}
---
+++ src/components/Users.tsx
@@ -0,0 +1,82 @@
+import { PortalNavbar } from "./PortalNavBar"
+import profilePictue from "../Image/me.jpg"
+import {RiDeleteBinLine} from "react-icons/ri"
+import {GrFormEdit} from "react-icons/gr"
+
+export const Users = () => {
+    return (
+        <>
+            <PortalNavbar/>
+            <div className="users">
+                <div className="users__container">
+                    <div className="users__title__button"> 
+                        <h3>USERS</h3>
+                        <button>Add New</button>
+                    </div>
+                    
+                    <div className="users__details">
+                        <div className="users__heading">
+                            <p>Full Name</p>
+                            <p>Mobile</p>
+                            <p>Designation</p>
+                            <p>Action</p>
+                        </div>
+                        <div className="users__details__roles">
+                            <div className="user__fullname__picture">
+                                <img className="navbar__profile-picture" src={profilePictue}/>
+                                <p>Lesego Kekana</p>
+                            </div>
+                            <p>082 123 4567</p>
+                            <p>Learner</p>
+                            <div className="users__actions">
+                                <RiDeleteBinLine/>
+                                <GrFormEdit/>
+                            </div>
+                           
+
+                        </div>
+                        <div className="users__details__roles">
+                            <div className="user__fullname__picture">
+                                <img className="navbar__profile-picture" src={profilePictue}/>
+                                <p>Lesego Kekana</p>
+                            </div>
+                            <p>082 123 4567</p>
+                            <p>Teacher</p>
+                            <div className="users__actions">
+                                <RiDeleteBinLine className="users__actions_delete"/>
+                                <GrFormEdit className="users__actions_edit"/>
+                            </div>
+
+                        </div>
+                        <div className="users__details__roles">
+                            <div className="user__fullname__picture">
+                                <img className="navbar__profile-picture" src={profilePictue}/>
+                                <p>Lesego Kekana</p>
+                            </div>
+                            <p>082 123 4567</p>
+                            <p>Alumni</p>
+                            <div className="users__actions">
+                                <RiDeleteBinLine/>
+                                <GrFormEdit/>
+                            </div>
+
+                        </div>
+                        <div className="users__details__roles">
+                            <div className="user__fullname__picture">
+                                <img className="navbar__profile-picture" src={profilePictue}/>
+                                <p>Lesego Kekana</p>
+                            </div>
+                            <p>082 123 4567</p>
+                            <p>Circuit Manager</p>
+                            <div className="users__actions">
+                                <RiDeleteBinLine/>
+                                <GrFormEdit/>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
