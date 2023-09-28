af67e023f54ed3cd3edc5cd5f2e4354e32df8a0a
Mduduzi-Jele <jelemduduzi@gmail.com>
2023-09-28 19:39:09 +0200

    Removed library

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,38 +1,30 @@
-import Masonry, {ResponsiveMasonry} from "react-responsive-masonry"
-
-const images =[
- "./src/assets/IMG_8007.JPG",
- "./src/assets/IMG_8135.JPG",
- "./src/assets/IMG_8169.JPG",
- "./src/assets/IMG_8722.JPG",
- "./src/assets/IMG_8724.JPG",
- "./src/assets/IMG_8726.JPG",
- "./src/assets/IMG_8731.JPG",
- "./src/assets/IMG_8006.JPG",
- "./src/assets/IMG_8006.JPG",
-
-]
+const images = [
+  "./src/assets/IMG_8007.JPG",
+  "./src/assets/IMG_8135.JPG",
+  "./src/assets/IMG_8169.JPG",
+  "./src/assets/IMG_8722.JPG",
+  "./src/assets/IMG_8724.JPG",
+  "./src/assets/IMG_8726.JPG",
+  "./src/assets/IMG_8731.JPG",
+  "./src/assets/IMG_8006.JPG",
+  "./src/assets/IMG_8006.JPG",
+];
 
 const Gallery = () => {
   return (
-    <div>
-        <h1>Gallery</h1>
-
-        <ResponsiveMasonry
-             columnsCountBreakPoints={{350 : 1 , 750:2, 900 : 3}}>
-          <Masonry>
-           {images.map((image, i ) => (
-            <img 
-            key={i}
-            src={image}
-            style={{width:"100%",display:"grid"}}
-            alt=""
-            />
-           ))} 
-            </Masonry> 
-        </ResponsiveMasonry>
-        </div>
+    <div className="gallery">
+      <h1>Gallery</h1>
+      {images.map((image, i) => (
+        <img
+          className="gallery__image"
+          key={i}
+          src={image}
+          style={{ width: "50%", display: "grid" }}
+          alt=""
+        />
+      ))}
+    </div>
   );
 };
 
-export default Gallery
+export default Gallery;
