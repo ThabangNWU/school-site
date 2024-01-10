40a265f079d207cdc26f48da9fe48ad0add2852c
Mduduzi-Jele <jelemduduzisa@gmail.com>
2024-01-10 15:22:16 +0200

    Did some stuff

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -2,32 +2,33 @@ import React from "react";
 import IMG_8007 from "../assets/IMG_8007.JPG"
 
 //
-const images = [
-  IMG_8007,
-  IMG_8007,
-  IMG_8007,
-  IMG_8007,
-  IMG_8007,
-  IMG_8007,
-  IMG_8007,
-  IMG_8007,
-  IMG_8007,
-  // "./src/assets/IMG_8135.JPG",
-  // "./src/assets/IMG_8169.JPG",
-  // "./src/assets/IMG_8722.JPG",
-  // "./src/assets/IMG_8724.JPG",
-  // "./src/assets/IMG_8726.JPG",
-  // "./src/assets/IMG_8731.JPG",
-  // "./src/assets/IMG_8006.JPG",
-  // "./src/assets/IMG_8006.JPG",
-];
+// const images = [
+//   IMG_8007,
+//   IMG_8007,
+//   IMG_8007,
+//   IMG_8007,
+//   IMG_8007,
+//   IMG_8007,
+//   IMG_8007,
+//   IMG_8007,
+//   IMG_8007,
+//   // "./src/assets/IMG_8135.JPG",
+//   // "./src/assets/IMG_8169.JPG",
+//   // "./src/assets/IMG_8722.JPG",
+//   // "./src/assets/IMG_8724.JPG",
+//   // "./src/assets/IMG_8726.JPG",
+//   // "./src/assets/IMG_8731.JPG",
+//   // "./src/assets/IMG_8006.JPG",
+//   // "./src/assets/IMG_8006.JPG",
+// ];
 
 const Gallery: React.FC = () => {
-  const first8Images = images.slice(0, 8);
+  // const first8Images = images.slice(0, 8);
 
   return (
    <div className="gallery">
-    <div className="gallery__heading">
+    <img src={IMG_8007} />
+    {/* <div className="gallery__heading">
         <h1>Gallery</h1>
     </div>
      <div className="image__grid">
@@ -35,7 +36,7 @@ const Gallery: React.FC = () => {
         <img key={image} src={image} alt="Image" />
       ))}
     </div>
-    <button className="gallery__button">See More</button>
+    <button className="gallery__button">See More</button> */}
    </div>
   );
 };
