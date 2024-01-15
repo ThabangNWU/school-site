859e7075680387c86aa58d12a7c9bcfd96c9e669
Mduduzi-Jele <jelemduduzisa@gmail.com>
2024-01-15 12:21:09 +0200

    Fixed image routes

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 
 const images = [
-  "../assets/IMG_8007.JPG",
-  "../assets/IMG_8135.JPG",
-  "../assets/IMG_8169.JPG",
-  "../assets/IMG_8722.JPG",
-  "../assets/IMG_8724.JPG",
-  "../assets/IMG_8726.JPG",
-  "../assets/IMG_8731.JPG",
-  "../assets/IMG_8006.JPG",
-  "../assets/IMG_8006.JPG"
+  "/assets/IMG_8007.JPG",
+  "/assets/IMG_8135.JPG",
+  "/assets/IMG_8169.JPG",
+  "/assets/IMG_8722.JPG",
+  "/assets/IMG_8724.JPG",
+  "/assets/IMG_8726.JPG",
+  "/assets/IMG_8731.JPG",
+  "/assets/IMG_8006.JPG",
+  "/assets/IMG_8006.JPG"
 ];
 
 const Gallery: React.FC = () => {
@@ -21,7 +21,7 @@ const Gallery: React.FC = () => {
         <h1>Gallery</h1>
     </div>
      <div className="image__grid">
-      {first8Images.map((image) => (
+      {images.map((image) => (
         <img key={image} src={image} alt="Image" />
       ))}
     </div>
