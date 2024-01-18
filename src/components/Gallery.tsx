94b08a3296c4a24d009795e15740735737f3183f
Mduduzi-Jele <jelemduduzisa@gmail.com>
2024-01-18 10:00:07 +0200

    fixed

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,18 +1,26 @@
 import React from "react";
-
-const images = [
-  "/assets/IMG_8007.JPG",
-  "/assets/IMG_8135.JPG",
-  "/assets/IMG_8169.JPG",
-  "/assets/IMG_8722.JPG",
-  "/assets/IMG_8724.JPG",
-  "/assets/IMG_8726.JPG",
-  "/assets/IMG_8731.JPG",
-  "/assets/IMG_8006.JPG",
-  "/assets/IMG_8006.JPG"
-];
+import IMG_8007 from "../assets/IMG_8007.JPG";
+import IMG_8135 from "../assets/IMG_8135.JPG";
+import IMG_8169 from "../assets/IMG_8169.JPG";
+import IMG_8722 from "../assets/IMG_8722.JPG";
+import IMG_8724 from "../assets/IMG_8724.JPG";
+import IMG_8726 from "../assets/IMG_8726.JPG";
+import IMG_8731 from "../assets/IMG_8731.JPG";
+import IMG_8006 from "../assets/IMG_8006.JPG";
 
 const Gallery: React.FC = () => {
+
+  const images = [
+    IMG_8007,
+    IMG_8135,
+    IMG_8169,
+    IMG_8722,
+    IMG_8724,
+    IMG_8726,
+    IMG_8731,
+    IMG_8006,
+  ];
+
   const first8Images = images.slice(0, 8);
 
   return (
@@ -21,8 +29,8 @@ const Gallery: React.FC = () => {
         <h1>Gallery</h1>
     </div>
      <div className="image__grid">
-      {first8Images.map((image) => (
-        <img key={image} src={image} alt="Image" />
+      {first8Images.map((image,index) => (
+        <img key={index} src={image} alt="Image" />
       ))}
     </div>
     <button className="gallery__button">See More</button>
--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  assetsInclude: ["**/*.JPG"],
   plugins: [react()],
   optimizeDeps: {
-    include: ['**/*.js', '**/*.ts', '**/*.jsx', '**/*.tsx', '**/*.vue', '**/*.jpg'],
+    include: ['**/*.js', '**/*.ts', '**/*.jsx', '**/*.tsx', '**/*.vue', '**/*.jpg', '**/*.JPG'],
 },
 server: {
   host: '0.0.0.0',
