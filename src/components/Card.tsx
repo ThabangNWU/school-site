b64c1b797d6da4166af3ab83f8f30fd87b2be82f
ThabangNWU <50927260+ThabangNWU@users.noreply.github.com>
2024-01-24 15:44:50 +0200

    Update Card.tsx

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,5 +1,11 @@
-import React from 'react';
-import IMG  from '../images/IMG.jpg';
+import React, { useState, useEffect } from 'react';
+import IMG from '../images/IMG.jpg';
+import axios from 'axios';
+
+interface Post {
+  postTitle: string;
+  postContent:string;
+}
 
 interface CardProps {
   imageUrl: string;
@@ -9,25 +15,53 @@ interface CardProps {
 }
 
 const Card: React.FC<CardProps> = ({ onReadMoreClick }) => {
+  const [posts, setPosts] = useState<Array<Post>>([]);
+  const [err, setError] = useState('');
+
+  const fetchPosts = () => {
+    getPosts()
+      .then(res => {
+        setPosts(res.data);
+        console.log(res.data);
+      })
+      .catch(err => {
+        setError(err.response.data.message);
+      });
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  const getPosts = async () => {
+    try {
+      return await axios.get("http://localhost:8080/api/auth/all/post");
+    } catch (e) {
+      throw e;
+    }
+  };
+
   return (
     <>
-    <div className="card">
-      <img src={IMG} alt="" className="card-image" />
-      <div className="card-content">
-        <div className='card-content-title'>
-          <h1 className="card-title">How I retain and remember what I study</h1>
-          {/* <div className='lines'></div> */}
-        </div>
-        <div className='text-body'>
-          <p className="card-body">Lorem ipsum dolor sit amet consectetur. Tincidunt sit et urna sit malesuada.Lorem ipsum dolor sit amet consectetur. Tincidunt sit et urna sit malesuada.</p>
-        </div>
-        <button className="read-more-button" onClick={onReadMoreClick}>
-          Read More
-        </button>
-      </div>
-    </div>
+      {posts.slice(0,3).map((post, id) => {
+        return (
+          <div className="card" key={id}>
+            <img src={IMG} alt="" className="card-image" />
+            <div className="card-content">
+              <div className='card-content-title'>
+                <h1 className="card-title">{post.postTitle}</h1>
+              </div>
+              <div className='text-body'>
+                <p className="card-body">{post.postContent}</p>
+              </div>
+              <button className="read-more-button" onClick={onReadMoreClick}>
+                Read More
+              </button>
+            </div>
+          </div>
+        );
+      })}
     </>
-    
   );
 };
 
