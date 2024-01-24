8a641d09768b06a23e1a806967ea7d484cb20985
ThabangNWU <50927260+ThabangNWU@users.noreply.github.com>
2024-01-24 15:42:27 +0200

    Update Blog.tsx

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -1,21 +1,59 @@
-// Blog.tsx
-import React from 'react';
 import '../css/Blog.css';
 import { useNavigate } from 'react-router-dom';
-import Card  from './Card'; 
-import { cardsData } from './CardData'; 
-
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+interface Post {
+  postTitle: string;
+  postContent:string;
+  postImage: string;
+  postId :number;
+}
 
 const Blog: React.FC = () => {
   const navigate = useNavigate();
-  
+  const [posts, setPosts] = useState<Array<Post>>([]);
+  const [err, setError] = useState('');
+  const [showFullContent, setShowFullContent] = useState<number | null>(null);
 
-  const onReadMoreClick = () => {
-    navigate('/label');
+  const fetchPosts = () => {
+    getPosts()
+      .then(res => {
+        setPosts(res.data);
+        console.log(res.data);
+      })
+      .catch(err => {
+        setError(err.response.data.message);
+      });
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  const getPosts = async () => {
+    try {
+      const response = await axios.get("http://localhost:8080/api/auth/all/post");
+      const postsWithImages = response.data.map((post) => {
+        const imageFileName = post.imageFileName;
+        console.log(imageFileName);
+        return { ...post, imageFileName };
+      });
+      setPosts(postsWithImages);
+      console.log(postsWithImages)
+    } catch (e) {
+      throw e;
+      console.log(err)
+    }
+  };
+
+
+  const onReadMoreClick = (postId: number) => {
+    // setShowFullContent(postId);
+    navigate(`/post/${postId}`)
   };
 
   const onMorePostsClick = () => {
-    navigate('/posts');
+    navigate("/posts");
   };
 
   return (
@@ -28,15 +66,33 @@ const Blog: React.FC = () => {
           <p className="text"> Lorem ipsum dolor sit amet consectetur. Lorem donec interdum elit sit porttitor et consequat. Urna ullamcorper suspendisse auctor lorem diam quis. Adipiscing donec ultrices at et non sed.</p>          
         </div>
         <div className="cards">
-          {cardsData.slice(0, 3).map((card, index) =>  (
-            <Card
-              key={index}
-              imageUrl={card.imageUrl}
-              title={card.title}
-              body={card.body}
-              onReadMoreClick={onReadMoreClick}
-            />
-          ))}
+          {posts.slice(0,3).map((post, id) => {
+            const imageUrl = `http://localhost:8080/api/auth/image/${post.postImage}`;
+            const truncatedContent =
+            post.postContent.length > 100
+              ? `${post.postContent.slice(0, 100)}...`
+              : post.postContent;
+        return (
+          <div className="card" key={id}>
+           <img src={imageUrl} alt="" className="card-image" />
+            <div className="card-content">
+              <div className='card-content-title'>
+                <h1 className="card-title">{post.postTitle}</h1>
+              </div>
+              <hr className='line_post'/>
+              <div className='text-body'>
+                <p className="card-body">{showFullContent === id ? post.postContent : truncatedContent}</p>
+              </div>
+              <button
+                    className="read-more-button"
+                    onClick={() => onReadMoreClick(post.postId)}
+                  >
+                   Read More
+                  </button>
+            </div>
+          </div>
+        );
+      })}
         </div>
         <button className="more-posts-button" onClick={onMorePostsClick}>
           MORE POSTS
